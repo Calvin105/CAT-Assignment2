e2eb0df8a1c77375e9000b754104859cc6475ada
Calvin Khoo <62873716+Calvin105@users.noreply.github.com>
2025-12-22 00:39:58 +0800

    feat: all pages start from top

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import OurTeam from "./pages/OurTeam";
@@ -10,6 +11,8 @@ import ServiceDetail from "./pages/ServiceDetail";
 function App() {
   return (
     <Router>
+      <ScrollToTop />
+      
       <div className="min-h-screen bg-[#EEE3C3] text-[#865832] font-sans selection:bg-[#C5A978] selection:text-[#865832] overflow-x-hidden flex flex-col justify-between">
         <Navbar />
         <Routes>
